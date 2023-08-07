31ae7904b5fc0770794c0d1bef282b39fe922041
proxy <141473110+proxyyz@users.noreply.github.com>
2023-08-07 07:26:29 -0500

    Updated spin.py

--- pleroma/commands/spin.py
+++ pleroma/commands/spin.py
@@ -2,7 +2,9 @@
 import random
 import os
 import requests
-from send_message import send_message
+from send_message import send_message 
+# qwq 
+
 def get_random_rarity():
     file_path = os.path.join(os.path.dirname(__file__), 'knives.txt')
     with open(file_path, 'r') as file:
@@ -11,20 +13,19 @@ def get_random_rarity():
 
 def handle_command(chat_id, auth_token, base_url, author_name):
     random_rarity = get_random_rarity()
-    knife_rarity = random_rarity.capitalize()  
+    knife_rarity = random_rarity.capitalize()
     
     message = f"Congratulations {author_name}! You got a {knife_rarity} for the Butterfly Knife!"
-    
-    # Add randomness to messages
-    if random.random() < 0.3:  # 30% chance for a trash message
-        message = f"What the scallop! You got trash! Better luck next time, {author_name}!"
+
+    if random.random() < 0.1:  # chance for a trash skin, 1 = 10%, 2 = 20%. etc
+        message = f"What the scallop, {author_name}. You got a less valuable skin."
     
     if random_rarity in ['Doppler', 'Ultraviolet', 'Lore', 'Crimson Red', 'Fade', 'Marble Fade']:
         message += f" Since it's rare, you get verified!"
         # Log to Discord webhook
         discord_webhook_url = ''
         log_data = {
-            'content': f"{author_name} spun a {knife_rarity} Butterfly Knife skin and won a rare skin!"
+            'content': f"{author_name} spun a {knife_rarity} for the Butterfly Knife!"
         }
         requests.post(discord_webhook_url, json=log_data)
     
